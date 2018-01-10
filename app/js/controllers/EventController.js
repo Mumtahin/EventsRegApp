726e8c954a1756f712c77da1f57884c537d2cace
Nazmul Islam Mumtahin <n.mumtahin1@gmail.com>
2018-01-10 19:52:14 +0000

    Created a node server for the EventData service which saves/retrieves event dsta as a json object

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,14 +3,15 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData) {
         $scope.sortorder = 'name';
-        $scope.event = eventData.event;
+        eventData.getEvent(function (event) {
+            $scope.event = event;
+        });
 
-        $scope.upVoteSession = function(session) {
+        $scope.upVoteSession = function (session) {
             session.upVoteCount++;
         };
 
-        $scope.downVoteSession = function(session) {
+        $scope.downVoteSession = function (session) {
             session.upVoteCount--;
         };
-    }
-);
+    });
